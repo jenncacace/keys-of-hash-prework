b10ada185be5f0a21f0daa6e53cd6f17dfe4938b
Jenn Cacace <jenn.cacace@gmail.com>
2018-01-21 17:22:56 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,8 @@ class Hash
   def keys_of(*arguments)
   keys_of_arg = []
     self.each do |key, value|
-    keys_of_arg << key if arguments.include?(value) 
+      keys_of_arg << key if arguments.include?(value) 
+    end 
     keys_of_arg
   end 
 end
